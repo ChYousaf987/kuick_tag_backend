ea89c1968276cd7108118a5781172311a567c674
Yousaf Awais <yousafawais987@gmail.com>
2025-05-26 16:22:00 +0500

    first commit

---
+++ middelwares/email.config.middelware.js
@@ -0,0 +1,15 @@
+// middleware/email.config.middleware.js
+
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: process.env.USER_AUTH_EMAIL,
+    pass: process.env.USER_AUTH_PASS,
+  },
+  pool: true,
+  maxConnections: 5,
+});
